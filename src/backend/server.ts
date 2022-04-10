32b1de58268475db531ff37f3067abb05f80caee
Sumukh Barve <8079593+sumukhbarve@users.noreply.github.com>
2022-04-11 00:39:13 +0530

    normalize watch and build mode location of frontend dir

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import { tapiduck } from 'monoduck'
-import { distParcelDir } from '../shared/dir-paths'
+import { distFrontendDir } from '../shared/dir-paths'
 import { api } from '../shared/endpoints'
 
 const app = express().use(cors(), express.json())
-app.use(express.static(distParcelDir))
+app.use(express.static(distFrontendDir))
 app.get('/', (_req, res) => res.redirect('/client.html'))
 
 tapiduck.route(app, api.ping, async function (reqdata) {
--- src/shared/dir-paths.ts
+++ src/shared/dir-paths.ts
@@ -1,9 +1,11 @@
 import path from 'path'
 
+// Repo root is 2 dirs up. Works with both watch-server & build-server scripts.
 const repoDir = path.resolve(__dirname, '../..')
+// console.log(`repoDir = ${repoDir}`)
+
 const srcDir = path.resolve(repoDir, 'src')
 const distDir = path.resolve(repoDir, 'dist')
-const distTscDir = path.resolve(repoDir, 'dist/tsc')
-const distParcelDir = path.resolve(repoDir, 'dist/parcel')
+const distFrontendDir = path.resolve(repoDir, 'dist/frontend')
 
-export { repoDir, srcDir, distDir, distTscDir, distParcelDir }
+export { repoDir, srcDir, distDir, distFrontendDir }
