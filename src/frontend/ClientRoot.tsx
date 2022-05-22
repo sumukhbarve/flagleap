3695b9b3d12c5d731a1a353617572f52fdf845eb
Sumukh Barve <8079593+sumukhbarve@users.noreply.github.com>
2022-05-23 00:46:28 +0530

    intro qs-based routing

--- src/frontend/ClientRoot.tsx
+++ src/frontend/ClientRoot.tsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { coRouteInfo, Link } from './lookableQs'
+import { lookduck } from 'monoduck'
+
+const useLookable = lookduck.makeUseLookable(React.useState, React.useEffect)
+
+const RouteAAA: React.VFC = function () {
+  return <h2>I am route AAA</h2>
+}
+
+const RouteBBB: React.VFC = function () {
+  return <h2>Ich bin Route BBB</h2>
+}
+
+const RouteCCC: React.VFC = function () {
+  return <h2>Mein hun route CCC</h2>
+}
+
+const routeMap: Record<string, React.ReactElement> = {
+  aaa: <RouteAAA />,
+  bbb: <RouteBBB />,
+  ccc: <RouteCCC />
+}
+
+const RouteNotFound: React.VFC = function () {
+  return <h2>Asa kuthlach route nahi (404ish)</h2>
+}
 
 const ClientRoot: React.VFC = function () {
+  const routeInfo = useLookable(coRouteInfo)
+  const routeEl = routeMap[routeInfo.id] ?? <RouteNotFound />
   return (
     <div className='clientRoot'>
       <h1>FlagLeap</h1>
-      <p>Leap forward with feature flags.</p>
+      <p>Mike check. Routing checking:</p>
+      <nav>
+        <Link to={{ id: 'aaa' }}>aaa</Link> |{' '}
+        <Link to={{ id: 'bbb' }}>bbb</Link> |{' '}
+        <Link to={{ id: 'ccc' }}>ccc</Link> |{' '}
+        <Link to={{ id: 'ddd' }}>ddd</Link>
+      </nav>
+      <hr />
+      {routeEl}
     </div>
   )
 }
---
+++ src/frontend/lookableQs.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { _, lookduck } from 'monoduck'
+
+// How it works:
+//  - When the page initially loads, data flows from URL --> App
+//  - As the user interacts with the app, data flows from App --> URL
+//  - As the user clicks back/forward, data flows from URL --> App
+//  - By `App`, we mean the observable `obQs` (and the linked `coRouteInfo`.)
+
+type RouteInfoType = {id: string} & Record<string, string>
+
+const windowLocation = globalThis.location
+const windowHistory = globalThis.history
+const windowAddEventListener = globalThis.addEventListener
+
+const prefixQmark = function (s: string): string {
+  if (s.startsWith('?')) { return s }
+  return '?' + s
+}
+const unprefixQmark = function (s: string): string {
+  if (s.startsWith('?')) { return s.slice(1) }
+  return s
+}
+
+const parseQs = function (qs: string): RouteInfoType {
+  const qsWoQmark = unprefixQmark(qs)
+  const parts = qsWoQmark.split('&')
+  const pairs = parts.map(s => s.split('=') as [string, string])
+  const routeInfo = _.fromPairs(pairs)
+  return { id: '', ...routeInfo }
+}
+const stringifyQs = function (routeInfo: RouteInfoType): string {
+  return _.toPairs(routeInfo).map(pair => `${pair[0]}=${pair[1]}`).join('&')
+}
+
+// Handle initial data flow from URL --> App with proper initialization:
+const obQs = lookduck.observable(unprefixQmark(windowLocation.search))
+// Handle data flow from App --> URL as user interacts with the App:
+obQs.subscribe(function () {
+  windowHistory.pushState(null, '', prefixQmark(obQs.get()))
+})
+// Handle data flow from URL --> App as the users navigates back/forward:
+windowAddEventListener('popstate', function () {
+  obQs.set(unprefixQmark(windowLocation.search))
+})
+
+const coRouteInfo = lookduck.computed(function (): RouteInfoType {
+  const qs = obQs.get()
+  return parseQs(unprefixQmark(qs))
+})
+const setRouteInfo = function (routeInfo: RouteInfoType): void {
+  obQs.set(unprefixQmark(stringifyQs(routeInfo)))
+}
+const useLookable = lookduck.makeUseLookable(React.useState, React.useEffect)
+const useRouteInfo = (): RouteInfoType => useLookable(coRouteInfo)
+
+const Link: React.FC<{to: RouteInfoType}> = function ({ to, children }) {
+  const href = prefixQmark(stringifyQs(to))
+  const onClick = function (event: React.MouseEvent): void {
+    event.preventDefault()
+    setRouteInfo(to)
+  }
+  return <a href={href} onClick={onClick}>{children}</a>
+}
+
+// Note: `obQs` is _not_ exported, while `coRouteInfo` is.
+export { coRouteInfo, useRouteInfo, setRouteInfo, Link }
